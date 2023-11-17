3a71569506c976e114ae2c08201b00d33702bf1a
sampath88 <sampathreddy8896@gmail.com>
2023-11-17 07:52:09 +0530

    tippy-test setup

---
+++ src/index.js
@@ -0,0 +1,11 @@
+console.log("tippyjs");
+
+// import { tippy } from "tippy2";
+import tippy from "tippy.js";
+import "tippy.js/dist/tippy.css";
+
+console.log(tippy);
+
+tippy("#hoverme",{
+    content: "hello world"
+});
